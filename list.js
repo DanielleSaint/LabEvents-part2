b38ca591d34d2835ba1e929969ef551743cf20b3
DanielleSaint <daniellesaint09@gmail.com>
2025-04-24 19:10:48 -0500

    Finalizada la tarea de Event LocalStorage

--- list.js
+++ list.js
@@ -1,72 +1,81 @@
-const botonAgregar = document.getElementById('botonTarea');
 
-//Cargar tareas
 document.addEventListener('DOMContentLoaded', function () {
+    const listado = document.getElementById('listado');
+    const botonAgregar = document.getElementById('botonTarea');
+    const borrarTodo = document.getElementById('borrarTodo');
+
+    //Cargar tareas
     let tareasGuardadas = JSON.parse(localStorage.getItem("ToDo")) || [];
-    tareasGuardadas.forEach( function (tarea) {
-        mostrarTareas(tarea.titulo, tarea.descripcionTarea);
+    tareasGuardadas.forEach(mostrarTareas)
+    //Agregar Tarea
+    botonAgregar.addEventListener('click', function (e) {
+        e.preventDefault();
+        //Traigo el valor de la tarea al agregar
+        const titulo = document.getElementById('nombreTarea').value.trim();
+        const descripcionTarea = document.getElementById('descripcion').value.trim();
+        if (!titulo || !descripcionTarea) {
+            alert("Completa este campo");
+            return;
+        }
+        //Crear Diccionario de la tarea
+        const tarea = {
+            id: Date.now(), // Agregar ID
+            titulo,
+            descripcionTarea,
+            completada: false
+        };
+        tareasGuardadas.push(tarea);
+        localStorage.setItem("ToDo", JSON.stringify(tareasGuardadas));
+        mostrarTareas(tarea);
+        //limpiar cuadros de tareas
+        document.getElementById('nombreTarea').value = '';
+        document.getElementById('descripcion').value = '';
     });
-})
-
-botonAgregar.addEventListener('click', function (e) {
-    e.preventDefault();
-    //Traigo el valor de la tarea al agregar
-    const titulo = document.getElementById('nombreTarea').value.trim();
-    const descripcionTarea = document.getElementById('descripcion').value.trim();
-    if (!titulo || !descripcionTarea) {
-        alert("Completa este campo");
-        return;
-    }
     
-    //guardar localstorage
-    const listaTareas = JSON.parse(localStorage.getItem("ToDo")) || [];
-    listaTareas.push({titulo, descripcionTarea});
-    localStorage.setItem("ToDo", JSON.stringify(listaTareas));
-    //limpiar cuadros de tareas
-    document.getElementById('nombreTarea').value = '';
-    document.getElementById('descripcion').value = '';
-
-    function mostrarTareas(titulo, descripcionTarea) {
+    //Mostrar Tarea
+    function mostrarTareas(tarea) {
         //Creo una nueva lista
         const nuevaLista = document.createElement('li');
         nuevaLista.className = 'listaToDo';
+        nuevaLista.dataset.id = tarea.id;
+        // Cambiar estado de imagen
+        const imagen = tarea.completada ? './img/png-transparent-check-mark-symbol-purple-steven-universe-amethyst-gemstone-logo-violet-circle-removebg-preview.png' : './img/pngtree-purple-round-border-image_2231965-removebg-preview.png';
+
         nuevaLista.innerHTML = `
             <div>
-                <img src="./img/pngtree-purple-round-border-image_2231965-removebg-preview.png" class="tareaCompletada" width=50px>
+                <img src="${imagen}" class="tareaCompletada" width=50px style="cursor:pointer;">
                 <div>
-                    <h2>${titulo}</h2>
-                    <p>${descripcionTarea}</p>
+                    <h2>${tarea.titulo}</h2>
+                    <p>${tarea.descripcionTarea}</p>
                 </div>
-                <button type=submit class="eliminar">Eliminar</button>
+                <button type=submit class="eliminar">Borrar</button>
             </div>
         `;
         // Cambiar imagen completada
         const img = nuevaLista.querySelector('.tareaCompletada');
         img.addEventListener('click', function () {
-            img.src = './img/png-transparent-check-mark-symbol-purple-steven-universe-amethyst-gemstone-logo-violet-circle-removebg-preview.png';
+            tarea.completada = !tarea.completada;
+            img.src = img.src = tarea.completada ? './img/png-transparent-check-mark-symbol-purple-steven-universe-amethyst-gemstone-logo-violet-circle-removebg-preview.png': './img/pngtree-purple-round-border-image_2231965-removebg-preview.png';
+            actualizarTarea(tarea); //local storage
         });
         // Eliminar tarea
         nuevaLista.querySelector('.eliminar').addEventListener('click', function () {
             nuevaLista.remove();
-            eliminarLocal(titulo, descripcionTarea);
+            tareasGuardadas = tareasGuardadas.filter(t => t.id !== tarea.id);
+            localStorage.setItem("ToDo", JSON.stringify(tareasGuardadas));
         });
-        document.getElementById('listado').appendChild(nuevaLista);
+        listado.appendChild(nuevaLista);
     };
-    mostrarTareas(titulo, descripcionTarea);
-
-    function eliminarLocal(titulo, descripcionTarea) {
-        const tareasGuardadas = JSON.parse(localStorage.getItem("ToDo")) || [];
-        tareasGuardadas = tareasGuardadas.filter(tarea =>
-            tarea.titulo !== titulo || tarea.descripcionTarea !== descripcionTarea
-        );
-        localStorage.setItem('ToDo', JSON.stringify(tareasGuardadas));
+    // Actualizar tarea en localStorage
+    function actualizarTarea(tareaActualizada) {
+        tareasGuardadas = tareasGuardadas.map(t => t.id === tareaActualizada.id ? tareaActualizada : t);
+        localStorage.setItem("ToDo", JSON.stringify(tareasGuardadas));
     }
+    // Borrar todo
+    borrarTodo.addEventListener('click', function () {
+        listado.innerHTML = '';
+        tareasGuardadas = [];
+        localStorage.removeItem('ToDo');
+    });
 });
-
-// Borrar todo
-const borrarTodo = document.getElementById("borrarTodo");
-borrarTodo.addEventListener('click', function () {
-    document.getElementById("listado").innerHTML = '';
-    localStorage.removeItem('ToDo');
-})
 
